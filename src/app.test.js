c704445628a2edbb7c66d3edc9ce639fe9d7cb9c
Matt Peck <matt@mortalife.com>
2023-10-19 11:38:34 +0100

    Failing test

---
+++ src/app.test.js
@@ -0,0 +1,33 @@
+import { describe, it, expect, beforeAll, afterAll } from "vitest";
+import app from "./app";
+
+const URL = process.env.DEV_URL || "http://localhost:3001";
+
+function getUrl(path) {
+  return `${URL}${path}`;
+}
+
+describe("app", () => {
+  beforeAll(() => {
+    app.listen(3001, () => {
+      console.log("Express App Listening on Port 3001");
+    });
+  });
+
+  afterAll(() => {
+    app.close();
+  });
+
+  it("it should not return the contract if it doesn't belong to the user", async () => {
+    /**
+     * 5 belongs to 3/8
+     */
+    const output = await fetch(getUrl("/contracts/5"), {
+      headers: {
+        profile_id: "3",
+      },
+    });
+
+    expect(output.status).toBe(404);
+  });
+});
